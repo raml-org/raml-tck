7fbc80e34b1482f28570b0b6fd907def6ea0d08c
KonstantinSviridov <ksviridov@gmail.com>
2016-09-09 17:51:56 +0600

    setting java parser branch to "master"
    fixing npm launching

--- src/install.js
+++ src/install.js
@@ -123,7 +123,7 @@ function npmInstall() {
         command += ".cmd";
     }
 
-    var installResult = spawnSync(command, ['install', '--prefix', destination], {stdio: [0, 1, 2]});
+    var installResult = spawnSync(command, ['install', '--prefix', destination], {cwd:destination,stdio: [0, 1, 2]});
 }
 
 function copySources() {
@@ -216,7 +216,7 @@ function cloneJavaParser() {
         fs.mkdirSync(javaRepoPath);
     }
     
-    spawnSync('git', ['clone', '-b', 'v2', '--depth', '1', 'https://github.com/raml-org/raml-java-parser.git', javaRepoPath], {stdio: [0, 1, 2]});
+    spawnSync('git', ['clone', '-b', 'master', '--depth', '1', 'https://github.com/raml-org/raml-java-parser.git', javaRepoPath], {stdio: [0, 1, 2]});
 }
 
 function mvnInstall() {
