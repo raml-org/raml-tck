e50b4d1b8e02df8a1607eec490f0e2125dfd996f
KonstantinSviridov <ksviridov@gmail.com>
2016-11-25 19:34:57 +0600

    Setting the "src/source" folder as default script argument.
    Adding an npm script

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 var args = process.argv;
 var fs = require("fs");
+var path = require("path");
 
-var processFolder = args[2];
+var processFolder;
+if(args.length > 2 ){
+  processFolder = args[2];
+}
+else{
+  var rootDir = process.cwd();
+  while(!fs.existsSync(path.resolve(rootDir,"package.json"))){
+    rootDir = path.resolve(rootDir,"../");
+  }
+  processFolder = path.resolve(rootDir,"src/source");
+}
 
 if(!fs.existsSync(processFolder)) {
   console.error("'" + processFolder + "' not found");
